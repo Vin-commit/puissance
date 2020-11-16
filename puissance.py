c636b0b0caa3352e7b7967b7c3eec86c0a21e587
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 19:16:08 +0100

    Add files via upload

---
+++ puissance.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/python3
+#coding: utf-8
+
+
+try:
+  x = int(input("Quel est le nombre de base : "))
+  y = int(input("Quelle est la puissance : "))
+  i = 1
+  cumul = x
+  while (i < y):
+    cumul = cumul * x
+    i += 1
+print (x, "^", y, "= "+str(cumul))
+except:
+  print("Veuillez saisir un nombre.")
+
+
+--------------------------------------------------------------------- Résultat --------------------------------------------------------------------------------
+Quel est le nombre de base : 10
+Quelle est la puissance : 3
+10 ^ 3 = 1000
